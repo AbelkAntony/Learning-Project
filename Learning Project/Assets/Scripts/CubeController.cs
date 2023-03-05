a4329f7ac0f31d695158f038347be09c6d1f4597
Aditya E Ajith <adityaeajith@gmail.com>
2023-03-05 19:59:07 +0530

    added timer movement

--- Learning Project/Assets/Scripts/CubeController.cs
+++ Learning Project/Assets/Scripts/CubeController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,16 +9,34 @@ public class CubeController : MonoBehaviour
     public float speed;
     public Vector3 moveDirection;
 
+    public float pauseTime;
+    public float pauseTimer = 0;
+
+    public float activeTime;
+    public float activeTimer = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(moveKey))
-        transform.position += moveDirection * speed * Time.deltaTime;
+		pauseTimer += Time.deltaTime;
+
+		if (pauseTimer >= pauseTime)
+        {
+            activeTimer += Time.deltaTime;
+            transform.position += speed * Time.deltaTime * moveDirection;
+
+            if(activeTimer >= activeTime)
+            {
+                pauseTimer = 0;
+                activeTimer = 0;
+            }
+        }
     }
+
 }
